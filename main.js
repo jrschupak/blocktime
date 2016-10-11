58534671a9927eb774e917501f7c9ed13d9f7507
Jonathan Schupak <jrschupak@gmail.com>
2016-10-11 16:18:29 -0400

    change timer content, added bpi

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 window.onload = function(){
 	console.log("Main.js is connected");
 
-var day = document.querySelector('.days');
-var hours = document.querySelector('.hours');
-var minutes = document.querySelector('.minutes');
-var seconds = document.querySelector('.seconds');
+var bst = document.querySelector('.bst');
+var dollar = document.querySelector('.dollars');
+var pound = document.querySelector('.pound');
+var euro = document.querySelector('.euro');
 
-console.log(day.innerText);
+console.log(bst.innerText);
 
 
 var timer = setInterval(function(){ //if needed this function is to make the timer tick
@@ -18,8 +18,11 @@ $.ajax({
 	url: "http://api.coindesk.com/v1/bpi/currentprice.json",
 	dataType: "JSON"
 }).done(function(response){
-	console.log(response.time)
-	day.innerText = response.time.updateduk;
+	console.log(response.bpi)
+	bst.innerText = response.time.updateduk;
+	dollar.innerHTML = "USD: " + response.bpi.USD.symbol + response.bpi.USD.rate;
+	euro.innerHTML = "Euro: " + response.bpi.EUR.symbol + response.bpi.EUR.rate;
+	pound.innerHTML = "Pound: " + response.bpi.GBP.symbol + response.bpi.GBP.rate;
 });
 
 } //end of window.onload

b23d49263d292197cf5110c0fbc310e85c32faed
Jonathan Schupak <jrschupak@gmail.com>
2016-10-11 13:19:14 -0400

    READNE.md update

--- README.md
+++ README.md
@@ -3,3 +3,22 @@
 Genesis Block Time (GBT) is time in blocks accepted on upon the Bitcoin blockchain. There is no better, true or secure way to tell time that Genesis Block Time (GBT).
 
 Slack: genesisblocktime.slack.com
+
+## Table of Contents
+
+## Bugs and features request
+
+## Contributing
+
+## Community
+
+## Creators
+ **Jonathan Schupak**
+
+ - <https://twitter.com/jrschupak>
+ - <https://github.com/jrschupak>
+
+ **Guy Lepage**
+
+ - <https://twitter.com/guylepage3>
+ - <https://github.com/guylepage3>
--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 window.onload = function(){
-	console.log("Main.js is connected")
+	console.log("Main.js is connected");
+
+var day = document.querySelector('.days');
+var hours = document.querySelector('.hours');
+var minutes = document.querySelector('.minutes');
+var seconds = document.querySelector('.seconds');
+
+console.log(day, hours, minutes, seconds.innerText);
+
+
+var timer = setInterval(function(){
+	// console.log("Set Interval timer for 1 second")
+	if(seconds.value === "59"){
+		
+	}
+}, 1000)
+
+
 }
+

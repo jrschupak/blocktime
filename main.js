d4907afa2cd1ee93c20d683ae40e12c1a2ec9c65
Jonathan Schupak <jrschupak@gmail.com>
2016-10-12 11:30:55 -0400

    copied ajax outside of setinterval to work on window onload

--- main.js
+++ main.js
@@ -8,6 +8,16 @@ var euro = document.querySelector('.euro');
 
 console.log(bst.innerText);
 
+$.ajax({
+	url: "http://api.coindesk.com/v1/bpi/currentprice.json",
+	dataType: "JSON"
+}).done(function(response){
+	console.log(response.bpi)
+	bst.innerText = response.time.updateduk;
+	dollar.innerHTML = "USD: " + response.bpi.USD.symbol + response.bpi.USD.rate;
+	euro.innerHTML = "Euro: " + response.bpi.EUR.symbol + response.bpi.EUR.rate;
+	pound.innerHTML = "Pound: " + response.bpi.GBP.symbol + response.bpi.GBP.rate;
+});
 
 var timer = setInterval(function(){ //if needed this function is to make the timer tick
 

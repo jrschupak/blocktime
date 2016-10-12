45935f268f18b048c96567eff92b043070bf28d4
Jonathan Schupak <jrschupak@gmail.com>
2016-10-12 11:58:07 -0400

    refactored ajax call so I can just call function)

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ var euro = document.querySelector('.euro');
 
 console.log(bst.innerText);
 
-$.ajax({
+var ajaxCall = function(){
+	$.ajax({
 	url: "http://api.coindesk.com/v1/bpi/currentprice.json",
 	dataType: "JSON"
 }).done(function(response){
@@ -18,19 +19,12 @@ $.ajax({
 	euro.innerHTML = "Euro: " + response.bpi.EUR.symbol + response.bpi.EUR.rate;
 	pound.innerHTML = "Pound: " + response.bpi.GBP.symbol + response.bpi.GBP.rate;
 });
+}
+
 
 var timer = setInterval(function(){ //if needed this function is to make the timer tick
 
-$.ajax({
-	url: "http://api.coindesk.com/v1/bpi/currentprice.json",
-	dataType: "JSON"
-}).done(function(response){
-	console.log(response.bpi)
-	bst.innerText = response.time.updateduk;
-	dollar.innerHTML = "USD: " + response.bpi.USD.symbol + response.bpi.USD.rate;
-	euro.innerHTML = "Euro: " + response.bpi.EUR.symbol + response.bpi.EUR.rate;
-	pound.innerHTML = "Pound: " + response.bpi.GBP.symbol + response.bpi.GBP.rate;
-});	
+ajaxCall();
 	
 }, 10000)
 

1a51530df1a1119ecbb792f1e25eeff25086d71a
Jonathan Schupak <jrschupak@gmail.com>
2016-10-12 20:05:56 -0400

    added ajax call to get balance of bitcoin address

--- main.js
+++ main.js
@@ -5,15 +5,16 @@ var bst = document.querySelector('.bst');
 var dollar = document.querySelector('.dollars');
 var pound = document.querySelector('.pound');
 var euro = document.querySelector('.euro');
+var address = '';
 
 console.log(bst.innerText);
 
-var ajaxCall = function(){
+var dateTimeCall = function(){
 	$.ajax({
 	url: "http://api.coindesk.com/v1/bpi/currentprice.json",
 	dataType: "JSON"
 }).done(function(response){
-	console.log(response.bpi)
+	console.log(response)
 	bst.innerText = response.time.updateduk;
 	dollar.innerHTML = "USD: " + response.bpi.USD.symbol + response.bpi.USD.rate;
 	euro.innerHTML = "Euro: " + response.bpi.EUR.symbol + response.bpi.EUR.rate;
@@ -21,11 +22,27 @@ var ajaxCall = function(){
 });
 }
 
-ajaxCall();
+
+var balanceCall = function(){
+	$.ajax({
+	// url: "https://blockexplorer.com/api/addr/" + address + "/balance",
+	url: "https://blockexplorer.com/api/addr/1DTRDHkWt3xyhrMCRHz1XV5DjCe9VxRoRW/balance",
+	dataType: "JSON"
+}).done(function(response){
+	console.log(response)
+})
+};
+
+
+
+
+
+balanceCall();
+dateTimeCall();
 
 var timer = setInterval(function(){ //if needed this function is to make the timer tick
 
-ajaxCall();
+dateTimeCall();
 	
 }, 10000)
 
